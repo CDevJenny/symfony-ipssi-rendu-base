fb675d8dc7f050bce620fe08a103fb9a6a4bfb45
sonny-ipssi <s.lallier@ecole-ipssi.net>
2022-12-03 21:55:54 +0100

    split app.css

--- assets/app.js
+++ assets/app.js
@@ -7,6 +7,10 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+import './styles/navbar.css';
+import './styles/form.css';
+import './styles/input.css';
+import './styles/table.css';
 
 // start the Stimulus application
 import './bootstrap';
